8c9b2dfb592027b8c59eb78e29729e6fe726a96c
CradleKing24 <jaronbturner@gmail.com>
2021-07-27 23:16:49 -0500

    fix teams displaying

--- src/Teams/Teams.js
+++ src/Teams/Teams.js
@@ -34,16 +34,32 @@ class Stuff extends Component {
 
   render() {
     const { error, isLoaded, teams } = this.state;
+    const teamNames = [];
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
+      teams.forEach((team) => {
+        let slimTeam = {};
+        if (team.metadata.team_name) {
+          slimTeam = {
+            locTeamName: team.metadata.team_name,
+            locUserId: team.user_id
+          };
+        } else {
+          slimTeam = {
+            locTeamName: team.display_name,
+            locUserId: team.user_id
+          };
+        }
+        teamNames.push(slimTeam);
+      });
       return (
         <ul>
-          {teams.map(item => (
+          {teamNames.map(item => (
             <li key={item.user_id}>
-              {item.metadata.team_name}
+              {item.locTeamName}
             </li>
           ))}
         </ul>
