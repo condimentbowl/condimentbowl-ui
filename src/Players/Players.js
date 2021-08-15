c6bbde97a42cbfae9e8db50d9c209ffe7c19100a
CradleKing24 <jaronbturner@gmail.com>
2021-08-15 17:24:11 -0500

    created players page, added example for looping through players json data

--- src/Players/Players.js
+++ src/Players/Players.js
@@ -1,74 +1,29 @@
 import React, { Component } from "react";
-import playerFile from "../../files/getAllPlayers.json"
+import playerFile from "./getAllPlayers.json"
 
-class Stuff extends Component {
+class Players extends Component {
   constructor(props) {
     super(props);
     this.state = {
       error: null,
       isLoaded: false,
-      players: []
+      players: {}
     };
   }
 
-//   componentDidMount() {
-//     fetch("https://api.sleeper.app/v1/league/699672633066725376/users")
-//       .then(res => res.json())
-//       .then(
-//         (result) => {
-//           this.setState({
-//             isLoaded: true,
-//             teams: result
-//           });
-//         },
-//         // Note: it's important to handle errors here
-//         // instead of a catch() block so that we don't swallow
-//         // exceptions from actual bugs in components.
-//         (error) => {
-//           this.setState({
-//             isLoaded: true,
-//             error
-//           });
-//         }
-//       )
-//   }
-
   render() {
-    // const { error, isLoaded, teams } = this.state;
-    // const teamNames = [];
-    // if (error) {
-    //   return <div>Error: {error.message}</div>;
-    // } else if (!isLoaded) {
-    //   return <div>Loading...</div>;
-    // } else {
-    console.log("player",playerFile.2103);
-      playerFile.forEach((player) => {
-          console.log(player.age);
-        // let slimTeam = {};
-        // if (team.metadata.team_name) {
-        //   slimTeam = {
-        //     locTeamName: team.metadata.team_name,
-        //     locUserId: team.user_id
-        //   };
-        // } else {
-        //   slimTeam = {
-        //     locTeamName: team.display_name,
-        //     locUserId: team.user_id
-        //   };
-        // }
-        // teamNames.push(slimTeam);
-      });
+    for (let [key, value] of Object.entries(playerFile)) {
+      console.log(value.age);
+    }
+
       return (
         <ul>
-          {teamNames.map(item => (
-            <li key={item.user_id}>
-              {item.locTeamName}
+            <li key="123">
+              boosh
             </li>
-          ))}
         </ul>
       );
-    // }
   }
 }
 
-export default Stuff;
+export default Players;
